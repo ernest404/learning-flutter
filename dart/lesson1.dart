42c7d0bbf583ca40bdbbb6abffde0f5bfe20c180
ernest404 <ernestmuisyoo@gmail.com>
2024-01-03 23:30:04 +0300

    Lesson 1 Hello world

---
+++ dart/lesson1.dart
@@ -0,0 +1,16 @@
+// 1. Install the Dart SDK on your computer.
+// 2. Write code
+// 3. Open Terminal
+// 4. navigate to the Directory that hosts your code
+// 5. Run code: dart run lesson1.dart
+
+// Single line comment
+/*
+Multiline 
+comment
+*/
+
+void main(List<String> args) {
+  //serves as the entry point for the execution of a Dart program
+  print("hello world"); //all expressions end with semi colon
+}
