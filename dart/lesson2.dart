20a6a200eaa2a740c71cf08264458d0a662565e8
ernest404 <ernestmuisyoo@gmail.com>
2024-01-03 23:56:38 +0300

    Lesson 2

--- dart/lesson2.dart
+++ dart/lesson2.dart
@@ -1,5 +1,11 @@
+// Variable declaration
 void main() {
-  var firstName = 'Ernest'; //Type inference from the data.
+  // Variable declaration with type inference.
+  // useful when the type is evident from the assigned value, and it helps to reduce redundancy in code.
+  var firstName = 'Ernest';
+
+  // Avoid var in cases where the type might not be obvious or when providing clear type information improves code readability.
+
   String lastName = 'Musyoki';
 
   print(firstName + ' ' + lastName);
