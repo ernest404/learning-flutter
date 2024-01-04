e7356c9a439ea16ad7e96e2fb4a00b73d28cf1a8
ernest404 <ernestmuisyoo@gmail.com>
2024-01-04 17:27:28 +0300

    Lesson 5: Constant and Final Variables

---
+++ dart/lesson5.dart
@@ -0,0 +1,22 @@
+// Constant & Final variables
+// Both are used to declare constants
+
+import 'dart:js_util';
+
+main() {
+  // The const keyword is used to declare compile-time constants. The value of a const variable must be known at compile-time.
+  const pi = 3.14;
+  print(pi);
+  // pi = 3; error
+
+  // The final keyword is used to declare a run-time constants. The value of a final variable is determined at runtime and can be assigned dynamically.
+
+  final x = add(5, 10);
+  print(x);
+  // x = 5; error
+
+  // const x = add(5, 10); not posible
+
+// const is also used to create immutable instances of classes when the class constructor is marked as const.
+// You cannot use const for non-constant values or values that are calculated at runtime.
+}
